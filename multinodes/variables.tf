5cf5286518874a9c8ef7f75e593241f7d9ab9a8a
ittiel <ittiel@gmail.com>
2019-08-06 17:51:20 +0300

    updating horizon AMI

--- multinodes/variables.tf
+++ multinodes/variables.tf
@@ -120,7 +120,7 @@ variable "test_core_5_ami" {
 
 variable "horizon_1_ami" {
   description = "Default Horizon-1 AMI"
-  default = "ami-0752ca2cd8405d12e"
+  default = "ami-071e45012e0a5f051"
 }
 
 variable "test_load_client_ami" {
