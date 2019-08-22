5ebccaf0f7fdeb67c68f7c64840d737a516c3994
test-name <test@email.com>
2019-08-22 11:05:54 +0300

    (Fix-Credentials-profile)

--- multinodes/main.tf
+++ multinodes/main.tf
@@ -5,6 +5,6 @@
 provider "aws" {
   region                  = "${var.aws_region}"
   shared_credentials_file = "${var.job_workspace}${var.job_folder}/credentials"
-  profile                 = "default"
+  profile                 = "multinode"
 }
 
